d85a95cc8362e2c16abc33da78acaad4598bd8da
Sergey <nakul.sv@gmail.com>
2019-07-10 14:46:24 +0000

    add question and answer models

---
+++ db/migrate/20190710142349_create_answers.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateAnswers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :answers do |t|
+      t.text :body
+      t.belongs_to :question, index: true
+
+      t.timestamps
+    end
+  end
+end
