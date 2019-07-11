ba4c363491d5c2badf5d586aa773e4b4f52b816f
Sergio <nakul.sv@gmail.com>
2019-07-11 08:52:06 +0300

    add dependent destroy for question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,5 @@
-# frozen_string_literal: true
-
 class Question < ApplicationRecord
-  has_many :answers
+  has_many :answers, dependent: :destroy
 
   validates :title, :body, presence: true
 end
--- db/migrate/20190710142349_create_answers.rb
+++ db/migrate/20190710142349_create_answers.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :answers do |t|
