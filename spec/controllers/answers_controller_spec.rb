6582bab7c9e42513313db8077217a1af293c54d5
Sergio <nakul.sv@gmail.com>
2019-07-13 08:51:01 +0300

    answers controller

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe AnswersController, type: :controller do
+  let(:question) { create :question }
+  let(:answer) { create :answer }
+
+  describe 'GET #new' do
+    before { get :new, params: { question_id: question } }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'assigns a new answer in @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'renders new' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new answer in the database' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(Answer, :count).by(1)
+      end
+
+      it 'redirects to show' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer) }
+        expect(response).to redirect_to assigns(:answer)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save a new answer in the database' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) } }.to_not change(Answer, :count)
+      end
+
+      it 'redirects to new' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+end
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :answer do
     body { 'MyText' }
-    question { '' }
+    question
+
+    trait :invalid do
+      body { nil }
+    end
   end
 end
