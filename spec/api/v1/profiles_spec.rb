30d64aa0b4b40327a5a6fc90524f382e44b33d37
Sergio <nakul.sv@gmail.com>
2019-09-06 09:01:38 +0300

    by lesson

---
+++ spec/api/v1/profiles_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.describe 'Profiles API', type: :request do
+  let(:headers) { { 'CONTENT-TYPE' => 'application/json', 'ACCEPT' => 'application/json' } }
+
+  describe 'GET /api/v1/profiles/me' do
+    let(:api_path) { '/api/v1/profiles/me' }
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
+    end
+
+    context 'authorized' do
+      let(:me) { create(:user) }
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+
+      before { get api_path, params: { access_token: access_token.token }, headers: headers }
+
+      it 'returns status 200' do
+        expect(response).to be_successful
+      end
+
+      it 'returns all public fields' do
+        %w[id email admin created_at updated_at].each do |attr|
+          expect(json[attr]).to eq me.send(attr).as_json
+        end
+      end
+
+      it 'does not returns private fields' do
+        %w[password encrypted_password].each do |attr|
+          expect(json).to_not have_key(attr)
+        end
+      end
+    end
+  end
+
+  describe 'GET /api/v1/profiles' do
+    let(:api_path) { '/api/v1/profiles' }
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
+    end
+
+    context 'authorized' do
+      let!(:users) { create_list(:user, 3) }
+      let!(:user) { create(:user, admin: true) }
+      let(:access_token) { create(:access_token, resource_owner_id: user.id) }
+
+      before { get api_path, params: { access_token: access_token.token }, headers: headers }
+
+      it 'returns status 200' do
+        expect(response).to be_successful
+      end
+
+      it 'returns list of users without authenticated user' do
+        expect(json.size).to eq 3
+      end
+
+      it 'returns list of non-admin users' do
+        json.each do |element|
+          expect(element['admin']).to_not eq true
+        end
+      end
+    end
+  end
+end
