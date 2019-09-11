8842bfd6a80a837f5b62b6955d5d7bc7603002e9
Sergey <nakul.sv@gmail.com>
2019-09-11 17:23:01 +0000

    pre-commit 3

--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Answers API', type: :request do
-  let(:headers) { { 'CONTENT-TYPE' => 'application/json', 'ACCEPT' => 'application/json' } }
-
-  describe 'GET /api/v1/answers/:id' do
+  let(:headers) { { 'ACCEPT' => 'application/json' } }
     let(:question) { create(:question) }
+    let(:access_token) { create(:access_token) }
+    
+    
+  describe 'GET /api/v1/answers/:id' do
     let(:first_file) { fixture_file_upload("#{Rails.root}/spec/rails_helper.rb", 'text/plain') }
     let(:second_file) { fixture_file_upload("#{Rails.root}/spec/spec_helper.rb", 'text/plain') }
     let(:answer) { create(:answer, files: [first_file, second_file], question: question) }
@@ -15,7 +17,6 @@ RSpec.describe 'Answers API', type: :request do
     end
 
     context 'authorized' do
-      let(:access_token) { create(:access_token) }
       let(:answer_response) { json['answer'] }
       let!(:comments) { create_list(:comment, 3, commentable: answer, user: create(:user)) }
       let!(:links) { create_list(:link, 4, linkable: answer) }
@@ -42,6 +43,26 @@ RSpec.describe 'Answers API', type: :request do
         %w[id body question_id].each do |attr|
           expect(answer_response[attr]).to eq answer.send(attr).as_json
         end
+      end
+    end
+  end
+  
+  describe 'POST /api/v1/answers' do
+    let(:api_path) { api_v1_question_answers_path(question) }
+    let(:method) { :post }
+    let(:valid_params) { { answer: attributes_for(:answer), access_token: access_token.token } }
+    let(:invalid_params) { { answer: attributes_for(:answer, :invalid), access_token: access_token.token } }
+
+    it_behaves_like 'API Authorizable'
+
+    context 'authorized' do
+
+      it 'with valid params create the answer' do
+        expect { post api_path, params: valid_params, headers: headers }.to change(Answer, :count).by(1)
+      end
+
+      it "with invalid params doesn't create the answer" do
+        expect { post api_path, params: invalid_params, headers: headers }.to_not change(Answer, :count)
       end
     end
   end
