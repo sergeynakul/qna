b27a41c71a814f6ba3960ae8490653bab187523c
Sergio <nakul.sv@gmail.com>
2019-09-12 08:48:05 +0300

    Add update and destroy for answer

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::AnswersController < Api::V1::BaseController
+  before_action :set_answer, except: :create
+
   def show
-    @answer = Answer.with_attached_files.find(params[:id])
     authorize! :read, @answer
     render json: @answer
   end
@@ -13,7 +14,21 @@ class Api::V1::AnswersController < Api::V1::BaseController
     @answer.save ? head(:ok) : head(422)
   end
 
+  def update
+    authorize! :update, @answer
+    @answer.update(answer_params) ? head(:ok) : head(422)
+  end
+
+  def destroy
+    authorize! :destroy, @answer
+    @answer.destroy
+  end
+
   private
+
+  def set_answer
+    @answer = Answer.with_attached_files.find(params[:id])
+  end
 
   def answer_params
     params.require(:answer).permit(:body)
--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe 'Answers API', type: :request do
   let(:headers) { { 'ACCEPT' => 'application/json' } }
   let(:question) { create(:question) }
   let(:access_token) { create(:access_token) }
+  let(:params) { { access_token: access_token.token } }
+  let(:valid_params) { { answer: attributes_for(:answer), access_token: access_token.token } }
+  let(:invalid_params) { { answer: attributes_for(:answer, :invalid), access_token: access_token.token } }
 
   describe 'GET /api/v1/answers/:id' do
     let(:first_file) { fixture_file_upload("#{Rails.root}/spec/rails_helper.rb", 'text/plain') }
@@ -20,7 +23,7 @@ RSpec.describe 'Answers API', type: :request do
       let!(:comments) { create_list(:comment, 3, commentable: answer, user: create(:user)) }
       let!(:links) { create_list(:link, 4, linkable: answer) }
 
-      before { get api_path, params: { access_token: access_token.token }, headers: headers }
+      before { get api_path, params: params, headers: headers }
 
       it_behaves_like 'API Lists' do
         let(:resource) { answer }
@@ -37,13 +40,56 @@ RSpec.describe 'Answers API', type: :request do
   describe 'POST /api/v1/answers' do
     let(:api_path) { api_v1_question_answers_path(question) }
     let(:method) { :post }
-    let(:valid_params) { { answer: attributes_for(:answer), access_token: access_token.token } }
-    let(:invalid_params) { { answer: attributes_for(:answer, :invalid), access_token: access_token.token } }
 
     it_behaves_like 'API Authorizable'
 
     it_behaves_like 'API create resource' do
       let(:resource) { Answer }
+    end
+  end
+
+  describe 'PUT /api/v1/answers/:id' do
+    let!(:answer) { create(:answer, user_id: access_token.resource_owner_id, question: question) }
+    let(:api_path) { api_v1_answer_path(answer) }
+    let(:method) { :put }
+
+    it_behaves_like 'API Authorizable'
+
+    context 'authorized' do
+      it 'with valid params update the answer' do
+        put api_path, params: valid_params, headers: headers
+
+        answer.reload
+
+        expect(answer.body).to eq valid_params[:answer][:body]
+      end
+
+      it 'with invalid params do not update the answer' do
+        put api_path, params: invalid_params, headers: headers
+
+        answer.reload
+
+        expect(answer.body).to_not eq invalid_params[:answer][:body]
+      end
+    end
+  end
+
+  describe 'DELETE /api/v1/answers/:id' do
+    let!(:answer) { create(:answer, user_id: access_token.resource_owner_id, question: question) }
+    let(:api_path) { api_v1_answer_path(answer) }
+    let(:method) { :delete }
+
+    it_behaves_like 'API Authorizable'
+
+    context 'authorized' do
+      it 'deletes the answer' do
+        expect { delete api_path, params: params, headers: headers }.to change(Answer, :count).by(-1)
+      end
+
+      it 'returns success status' do
+        delete api_path, params: params, headers: headers
+        expect(response).to be_successful
+      end
     end
   end
 end
